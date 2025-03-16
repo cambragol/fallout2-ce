d81bae8d377f469e4d86924570756b452fcbcd3c
cambragol <griffon_academy@yahoo.ca>
2025-03-16 11:31:58 +0900

    fix to set brightness before showing Splash Screens

--- src/game.cc
+++ src/game.cc
@@ -164,6 +164,14 @@ int gameInitWithOptions(const char* windowTitle, bool isMapper, int font, int a4
     // SFALL: Allow to skip splash screen
     int skipOpeningMovies = 0;
     configGetInt(&gSfallConfig, SFALL_CONFIG_MISC_KEY, SFALL_CONFIG_SKIP_OPENING_MOVIES_KEY, &skipOpeningMovies);
+    
+    // load preferences before Splash screen to get proper brightness
+    if (_init_options_menu() != 0) {
+        debugPrint("Failed on init_options_menu\n");
+        return -1;
+    }
+
+    debugPrint(">init_options_menu\n");
 
     if (!gIsMapper && skipOpeningMovies < 2) {
         showSplash();
@@ -337,13 +345,6 @@ int gameInitWithOptions(const char* windowTitle, bool isMapper, int font, int a4
     }
 
     debugPrint(">scr_disable\t");
-
-    if (_init_options_menu() != 0) {
-        debugPrint("Failed on init_options_menu\n");
-        return -1;
-    }
-
-    debugPrint(">init_options_menu\n");
 
     if (endgameDeathEndingInit() != 0) {
         debugPrint("Failed on endgameDeathEndingInit");
