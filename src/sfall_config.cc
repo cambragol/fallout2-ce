6f7d38bb9b19a8fa4d0d41a647538a477210e2f9
cambragol <griffon_academy@yahoo.ca>
2025-05-03 07:42:19 +0900

    Added autoOpenDoors
    Controlled via Sfall (ddraw.ini)

--- src/sfall_config.cc
+++ src/sfall_config.cc
@@ -66,6 +66,7 @@ bool sfallConfigInit(int argc, char** argv)
     configSetString(&gSfallConfig, SFALL_CONFIG_SCRIPTS_KEY, SFALL_CONFIG_GLOBAL_SCRIPT_PATHS, "");
 
     configSetInt(&gSfallConfig, SFALL_CONFIG_MISC_KEY, SFALL_CONFIG_PIPBOY_AVAILABLE_AT_GAMESTART, 0);
+    configSetInt(&gSfallConfig, SFALL_CONFIG_MISC_KEY, SFALL_CONFIG_AUTO_OPEN_DOORS, 0);
 
     char path[COMPAT_MAX_PATH];
     char* executable = argv[0];
