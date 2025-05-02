06c809dae3108af82330d9fba38c456fdd34878f
cambragol <griffon_academy@yahoo.ca>
2025-05-02 21:54:06 +0900

    platform addition

---
+++ src/platform/git_version.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define _BUILD_AUTHOR "Vasilii Rogin"
+#define _BUILD_BRANCH "main"
+#define _BUILD_HASH   "04b8c84"
+#define _BUILD_VER    "continious"
+#define _BUILD_DATE   "Jan 14 2025 18:18:05"
+
+#define CI_BUILD 0
