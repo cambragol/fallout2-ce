158468275d1dc55f8fb96645c7bbb880700ce609
cambragol <griffon_academy@yahoo.ca>
2025-07-19 18:37:14 +0900

    Update git_version.h

--- src/platform/git_version.h
+++ src/platform/git_version.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #define _BUILD_AUTHOR "cambragol"
-#define _BUILD_BRANCH "test-import"
-#define _BUILD_HASH "70fff85"
-#define _BUILD_VER "continious"
-#define _BUILD_DATE "Jun 08 2025 20:05:21"
+#define _BUILD_BRANCH "widescreen"
+#define _BUILD_HASH   "b730667"
+#define _BUILD_VER    "continious"
+#define _BUILD_DATE   "Jul 19 2025 10:59:57"
 
 #define CI_BUILD 0
